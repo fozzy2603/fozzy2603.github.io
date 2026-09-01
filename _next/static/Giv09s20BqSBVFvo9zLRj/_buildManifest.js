45dc2d626870359b5d0544b2d04870cf2a5e9262
bitbucket-pipelines <commits-noreply@bitbucket.org>
2026-09-01 21:28:26 +0000

    update CV

---
+++ _next/static/Giv09s20BqSBVFvo9zLRj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-38841059706dcc6c.js","static/chunks/pages/index-3f28578e20526601.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- components/Summary.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Summary = ({text}:{text: string}) => {
-    return (
-        <div className="information-box">
-            {text}
-        </div>
-    );
-};
